629a7ca8d7918579f61f82e6553bf9a18e549ca7
Achyut <aoogleak@gmail.com>
2022-11-12 15:06:50 +0545

    1.3 jwt verification and cookie

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,22 +1,47 @@
 const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	tokens: [{ token: { type: String, required: true } }],
 });
+
+// MIDDLEWARE
+//pre    works before save() method for db is called and pw is modified
+userSchema.pre("save", async function (next) {
+	// donot use fat arrow function when deal with this.
+	if (this.isModified("password")) {
+		this.password = await bcrypt.hash(this.password, 10);
+	}
+	next();
+});
+
+// JWT token verification
+userSchema.methods.generateAuthToken = async function () {
+	try {
+		let sendToken = jwt.sign({ _id: this._id }, process.env.AUTH_SECRET);
+		this.tokens = this.tokens.concat({ token: sendToken });
+		await this.save();
+		return sendToken;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const User = mongoose.model("USER", userSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,55 +1,91 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 require("../db/connection");
 const User = require("../model/userSchema");
+const { createIndexes } = require("../model/userSchema");
 
 // Middleware
-const middleware = (req, res, next) => {
-  next();
-};
+// const middleware = (req, res, next) => {
+//   next();
+// };
 
 router.get("/", (req, res) => {
-  res.send("Starting Router!");
+	res.send("Starting Router!");
 });
 
 router.post("/register", async (req, res) => {
-  const { name, email, phone, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(422).json({ message: "Please fill required fields." });
-  }
+	const { name, email, phone, password } = req.body;
+	if (!name || !email || !password) {
+		return res
+			.status(422)
+			.json({ message: "Please fill required fields." });
+	}
 
-  try {
-    const userExist = await User.findOne({ email: email });
-    if (userExist) {
-      return res.status(422).json({ message: "User Email already in use." });
-    }
+	try {
+		const userExist = await User.findOne({ email: email });
+		if (userExist) {
+			return res
+				.status(422)
+				.json({ message: "User Email already in use." });
+		}
 
-    const user = new User({ name, email, phone, password });
-
-    const registered = await user.save();
-    if (registered) {
-      res.status(201).json({ message: "user successfully registered." });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Failed to register." });
-  }
+		const user = new User({ name, email, phone, password });
+		// Middleware Hash happens here
+		const registered = await user.save();
+		if (registered) {
+			res.status(201).json({ message: "user successfully registered." });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Failed to register." });
+	}
 });
 
-router.get("/aboutme", middleware, (req, res) => {
-  res.send("About Me");
+router.post("/signin", async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		if (!email || !password) {
+			return res
+				.status(422)
+				.json({ message: "Please fill required fields." });
+		}
+		const userExist = await User.findOne({ email: email });
+		if (userExist) {
+			const verifiedPassword = await bcrypt.compare(
+				password,
+				userExist.password
+			);
+			if (verifiedPassword) {
+				// jWT token generate
+				const getToken = await userExist.generateAuthToken();
+				console.log(getToken);
+				res.cookie("Jwt Cookie", getToken, {
+					expires: new Date(Date.now() + 259200000), // 3 days in miliseconds
+					httpOnly: true,
+				});
+
+				res.send("login successfull");
+			}
+		} else {
+			res.status(500).json({ message: "Invalid Credentials." });
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
 
-router.get("/signin", (req, res) => {
-  res.send("Login page");
+router.get("/aboutme", (req, res) => {
+	res.send("About Me");
 });
 
 router.get("/signup", (req, res) => {
-  res.send("Create account page");
+	res.send("Create account page");
 });
 
 router.get("/settings", (req, res) => {
-  res.send("settings");
+	res.send("settings");
 });
 
 module.exports = router;
