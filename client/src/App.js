0e5b62ba6bce2bf0b75900bf81015b2602ed2fdb
Achyut <aoogleak@gmail.com>
2022-11-15 15:27:05 +0545

    1.5 react pages routing configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="signup" element={<SignUp />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
 		</div>
 	);
 }
