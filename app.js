0b131377ea4b9c679e2056ff39f4644ab207359d
Achyut <aoogleak@gmail.com>
2023-02-14 17:47:40 +0545

    2.2 fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(require("./router/auth"));
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build/index.html"));
+	app.use(express.static("./client/build/index.html"));
 }
 
 app.listen(process.env.PORT, () => {
